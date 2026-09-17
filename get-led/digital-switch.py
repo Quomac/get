df44b500c548560742a3f30882f6aa564105a698
quomac <mentsig@gmail.com>
2026-09-17 16:48:17 +0300

    raspberry second task code adding

---
+++ get-led/digital-switch.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as gpio
+import time
+
+gpio.setmode(gpio.BCM)
+
+led = 26
+
+bt = 13
+
+state = 0
+
+gpio.setup(bt, gpio.IN)
+gpio.setup(led, gpio.OUT)
+
+while True:
+    if gpio.input(bt) == 1:
+        state = not state
+        gpio.output(led, state)
+        time.sleep(0.2)
