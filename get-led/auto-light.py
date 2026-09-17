7d1733a379830e5167a387edb1871c3610ae2444
quomac <mentsig@gmail.com>
2026-09-17 17:11:30 +0300

    raspberry few tasks for leds

---
+++ get-led/auto-light.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as gpio
+import time
+
+gpio.setmode(gpio.BCM)
+
+led = 26
+ls = 13 
+
+gpio.setup(led, gpio.OUT)
+gpio.setup(ls, gpio.IN)
+
+while True:
+    if gpio.input(bt) == 1:
+        state = 1
+    else:
+        state = 0
+    gpio.output(led, state)
+    time.sleep(0.2)
