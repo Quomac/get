e0f0eb6e4d416332a617f95a48d10677123945da
Quomac <mentsig@gmail.com>
2026-09-24 17:43:59 +0300

    dac 2-nd fix

--- get-dac/r2r-dac.py
+++ get-dac/r2r-dac.py
@@ -14,6 +14,7 @@ class R2R_DAC:
         gpio.cleanup()
     
     def set_number(self, number):
+        print([int(element) for element in bin(number)[2:].zfill(8)], number)
         gpio.output(self.gpio_bits, [int(element) for element in bin(number)[2:].zfill(8)])
 
     def set_voltage(self, voltage):
@@ -23,6 +24,7 @@ class R2R_DAC:
             self.set_number(0)
 
         self.set_number(int(voltage / self.dynamic_range * 255))
+        print(111)
 
 
 
